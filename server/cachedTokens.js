94a68aa88dfae049ab94d5f1ee3cbd50e8d46b60
alvy <alvyjudy@gmail.com>
2020-09-07 15:52:27 -0400

    cached tokens module ready (with test)

---
+++ server/cachedTokens.js
@@ -0,0 +1,29 @@
+const CachedTokens = () => {
+  
+  const tokens = [] //{userID, tokenValue, expiry}
+
+  const get = (userID) => {
+    return instance.tokens.filter(each => {
+      return userID === each.userID
+    })[0] || undefined
+  };
+
+  const add = (token) => {
+    instance.tokens.push(token)
+  }
+
+  const rm = (userID) => {
+    instance.tokens = instance.tokens.filter(
+      each => userID !== each.userID
+    )
+  }
+
+  const flush = () => {
+    instance.tokens = []
+  }
+
+  const instance = {tokens, get, add, rm, flush}
+  return instance
+}
+
+module.exports = CachedTokens;
---
+++ tests/tokenCache.unit.test.js
@@ -0,0 +1,35 @@
+const CachedTokens = require("../server/cachedTokens.js")
+const cachedTokens = CachedTokens();
+
+afterEach(()=>{
+  cachedTokens.flush()
+})
+
+test("initial empty", ()=>{
+  expect(cachedTokens.tokens).toEqual([]);
+})
+
+test("flush", ()=>{
+  cachedTokens.tokens = 'stuff'
+  expect(cachedTokens.tokens).toEqual('stuff');
+  cachedTokens.flush();
+  expect(cachedTokens.tokens).toEqual([]);
+})
+
+test("add", ()=>{
+  const token1 = {userID: 1, tokenValue: 'abc', expiry: 100}
+  const token2 = {userID: 2, tokenValue: 'ddssa', expiry: 1000}
+  cachedTokens.add(token1)
+  cachedTokens.add(token2)
+  expect(cachedTokens.get(token1.userID)).toBe(token1)
+})
+
+test("remove", ()=>{
+  const token1 = {userID: 1, tokenValue: 'abc', expiry: 100}
+  const token2 = {userID: 2, tokenValue: 'ddssa', expiry: 1000}
+  cachedTokens.add(token1)
+  cachedTokens.add(token2)
+  cachedTokens.rm(token2.userID)
+  console.log(cachedTokens.tokens)
+  expect(cachedTokens.tokens).toEqual([token1])
+})
