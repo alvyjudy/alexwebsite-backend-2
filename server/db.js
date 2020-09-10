4da2b9f7896b8b84350be9ca856b31d2087dd6d0
alvy <alvyjudy@gmail.com>
2020-09-10 10:27:52 -0400

    logging support for query

--- server/db.js
+++ server/db.js
@@ -1,4 +1,21 @@
 const {Pool} = require("pg");
 let pool = new Pool();
 
-module.exports = pool;
+module.exports = {
+  query: async (text, params) => {
+    let res;
+    try {
+      res = await pool.query(text, params);
+    } catch (e) {
+      console.log("Query failed to execute:", 
+        "\u001b[31;1m", text, "\u001b[0;m",
+        "params:", params, "\n\nError message", e);
+    } finally {
+      return res
+    }
+  },
+  connect: ()=> {
+    return pool.connect()
+  },
+  end: ()=>{pool.end()}
+}
