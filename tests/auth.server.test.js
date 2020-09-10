e739254a64145b8af1a7d6d998a3f2e5297857d0
alvy <alvyjudy@gmail.com>
2020-09-10 09:47:30 -0400

    WIP server end to end tests

---
+++ tests/auth.server.test.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const {createHttpTerminator} = require("http-terminator");
+const create = require("../database/create.js");
+const user = require("../database/user.js");
+const path = require("path");
+
+
+require("dotenv").config({path:path.resolve(path.dirname(__filename), ".env")});
+const app = require("../server/app.js");
+const pool = require("../server/db.js");
+const server = app.listen();
+const httpTerminator = createHttpTerminator({server});
+const ENDPOINT = "http://localhost:" + server.address().port;
+const SCHEMA = "test_setup";
+
+beforeAll(async ()=>{
+  const client = await pool.connect();
+  client.query(`DROP SCHEMA IF EXISTS ${SCHEMA} CASCADE;`);
+  client.query(`CREATE SCHEMA ${SCHEMA};`);
+  client.query(`SET search_path TO ${SCHEMA}`);
+  Object.values(create).forEach(
+    (action) => {client.query(action);}
+  ) 
+  await client.release();
+})
+
+afterAll(async ()=>{
+  const client = await pool.connect();
+  await client.query(`DROP SCHEMA IF EXISTS ${SCHEMA} CASCADE;`);
+  await client.release();
+  httpTerminator.terminate();
+  pool.end()
+})
+
+test("", async ()=>{
+  const res = (await axios.get(ENDPOINT + "/time"))
+  const status = res.status;
+  const data = res.data;
+  console.log(status, data);
+})
+
